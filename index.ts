e19b84360c072c04bc52843174576b0acf138260
liuxueyong <liuxueyong@agora.io>
2021-09-03 14:23:23 +0800

    去除晚饭提醒

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import schedule from 'node-schedule';
 import alertDinner from './function/alertDinner';
 
-schedule.scheduleJob('00 30 17 * * 1-5', () => {
-  alertDinner();
-});
+// schedule.scheduleJob('00 30 17 * * 1-5', () => {
+//   alertDinner();
+// });
